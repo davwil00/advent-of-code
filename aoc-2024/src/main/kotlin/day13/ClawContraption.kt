d8e8a79763ff916587d63e7508e671dd531be38b
David Williams <14893549+davwil00@users.noreply.github.com>
2024-12-15 12:12:28 +0000

    Nope, day 12 is actually day 13

---
+++ aoc-2024/src/main/kotlin/day13/ClawContraption.kt
@@ -0,0 +1,75 @@
+package day13
+
+import utils.LongCoordinate
+import utils.readInputLines
+import kotlin.math.round
+
+class ClawContraption(input: List<String>) {
+    private val buttonRegex = Regex("""Button [AB]: X\+(\d+), Y\+(\d+)""")
+    private val prizeRegex = Regex("""Prize: X=(\d+), Y=(\d+)""")
+
+    val machineButtonBehaviours = parseInput(input)
+
+    fun parseInput(input: List<String>): List<MachineButtonBehaviour> {
+        return input.chunked(4).map { def ->
+            val (ax, ay) = buttonRegex.matchEntire(def[0])!!.destructured
+            val (bx, by) = buttonRegex.matchEntire(def[1])!!.destructured
+            val (px, py) = prizeRegex.matchEntire(def[2])!!.destructured
+
+            MachineButtonBehaviour(
+                LongCoordinate(ax.toLong(), ay.toLong()),
+                LongCoordinate(bx.toLong(), by.toLong()),
+                LongCoordinate(px.toLong(), py.toLong())
+            )
+        }
+    }
+
+    fun part1(): Long {
+        return machineButtonBehaviours.sumOf {
+            it.calculateCost(false)
+        }
+    }
+
+    fun part2(): Long {
+        return machineButtonBehaviours.sumOf {
+            it.calculateCost(true)
+        }
+    }
+
+    data class MachineButtonBehaviour(
+        val buttonA: LongCoordinate,
+        val buttonB: LongCoordinate,
+        val prizeLocation: LongCoordinate
+    ) {
+        /**
+         * Uses the cross multiplication rule to find the intersection
+         * (b1c2-b2c1)/(a1b2-a2b1), (c1a2-c2a1)/a1b2-a2b1)
+         */
+        private fun findIntersection(withUnitConversionError: Boolean): Pair<Long, Long>? {
+            val target = if (withUnitConversionError) { // to get in the form ax + by + c = 0
+                (prizeLocation + LongCoordinate(10000000000000, 10000000000000)) * -1
+            } else {
+                prizeLocation * -1
+            }
+            val buttonAPresses =
+                ((buttonB.x * target.y) - (buttonB.y * target.x)).toDouble() / ((buttonA.x * buttonB.y) - (buttonA.y * buttonB.x))
+            val buttonBPresses =
+                ((target.x * buttonA.y) - (target.y * buttonA.x)).toDouble() / ((buttonA.x * buttonB.y) - (buttonA.y * buttonB.x))
+            if (round(buttonAPresses) != buttonAPresses || round(buttonBPresses) != buttonBPresses) {
+                return null
+            }
+            return Pair(buttonAPresses.toLong(), buttonBPresses.toLong())
+        }
+
+        fun calculateCost(withUnitConversionError: Boolean): Long {
+            val intersection = findIntersection(withUnitConversionError)
+            return if (intersection == null) 0 else intersection.first * 3 + intersection.second * 1
+        }
+    }
+}
+
+fun main() {
+    val clawcontraption = ClawContraption(readInputLines(13))
+    //println(clawcontraption.part1())
+    //println(clawcontraption.part2())
+}
--- aoc-2024/src/test/kotlin/day12/GardenGroupsTest.kt
+++ aoc-2024/src/test/kotlin/day12/GardenGroupsTest.kt
@@ -1,51 +1,26 @@
 package day12
 
-import org.assertj.core.api.Assertions.assertThat
+import day13.ClawContraption
 import org.junit.jupiter.api.Test
-import utils.readInputLines
 
 class GardenGroupsTest {
-    private val testInput = """
-        Button A: X+94, Y+34
-        Button B: X+22, Y+67
-        Prize: X=8400, Y=5400
+    private val testInput = """""".lines()
 
-        Button A: X+26, Y+66
-        Button B: X+67, Y+21
-        Prize: X=12748, Y=12176
-
-        Button A: X+17, Y+86
-        Button B: X+84, Y+37
-        Prize: X=7870, Y=6450
-
-        Button A: X+69, Y+23
-        Button B: X+27, Y+71
-        Prize: X=18641, Y=10279
-    """.trimIndent().lines()
-
-    private val testSubject = GardenGroups(testInput)
+    private val testSubject = ClawContraption(testInput)
 
     @Test
-    fun `should find costs for sample input`() {
-        assertThat(testSubject.part1())
-            .isEqualTo(480)
-    }
-
-    @Test
-    fun `should find costs with error for sample input`() {
-        assertThat(testSubject.part2())
-            .isEqualTo(480)
+    fun `should`() {
     }
 
     @Test
     fun `should get correct answer for part 1`() {
-         assertThat(GardenGroups(readInputLines(12)).part1())
-            .isEqualTo(36758)
+        // assertThat(ClawContraption(readInputLines(13)).part1())
+        //    .isEqualTo()
     }
 
     @Test
     fun `should get correct answer for part 2`() {
-         assertThat(GardenGroups(readInputLines(12)).part2())
-            .isEqualTo(76358113886726)
+        // assertThat(ClawContraption(readInputLines(13)).part2())
+        //    .isEqualTo()
     }
 }
