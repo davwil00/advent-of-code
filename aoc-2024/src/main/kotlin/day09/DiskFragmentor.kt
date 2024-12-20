74f850eb187129094b062a69ed2c22f0b2a64c05
David Williams <davwil00@gmail.com>
2024-12-20 14:54:58 +0100

    Day 9 part 2

--- aoc-2024/src/main/kotlin/day09/DiskFragmentor.kt
+++ aoc-2024/src/main/kotlin/day09/DiskFragmentor.kt
@@ -11,16 +11,29 @@ class DiskFragmentor(private val input: String) {
         return block.splitToString().flatMapIndexed { idx, count ->
             val blockCount = count.toInt()
             if (idx.isEven()) {
-                List(blockCount) { IdBlock(idx/2L) }
+                List(blockCount) { IdBlock(idx / 2L) }
             } else {
-                List(blockCount) {FreeSpaceBlock() }
+                List(blockCount) { FreeSpaceBlock }
             }
         }
     }
 
-    fun runFragmenter(blocks: List<Block>): List<Block> {
-        val compactedList = LinkedList<Block>(blocks)
-        while(compactedList.count { it is FreeSpaceBlock } > 0 ) {
+    private fun expandBlocksWithSize(block: String): List<BlockWithSize> {
+        return block.splitToString().mapIndexedNotNull { idx, count ->
+            val blockCount = count.toInt()
+            if (blockCount != 0) {
+                if (idx.isEven()) {
+                    IdBlockWithSize(idx / 2L, blockCount)
+                } else {
+                    FreeSpaceBlockWithSize(blockCount)
+                }
+            } else null
+        }
+    }
+
+    fun runFragmentor(blocks: List<Block>): List<Block> {
+        val compactedList = LinkedList(blocks)
+        while (compactedList.count { it is FreeSpaceBlock } > 0) {
             val currentBlock = compactedList.removeLast()
             if (currentBlock !is FreeSpaceBlock) {
                 val spaceIdx = compactedList.indexOfFirst { it is FreeSpaceBlock }
@@ -30,26 +43,108 @@ class DiskFragmentor(private val input: String) {
         return compactedList
     }
 
+    private tailrec fun runFragmentor(blocks: LinkedList<BlockWithSize>, id: Long): List<BlockWithSize> {
+        if (id == 0L) {
+            return blocks
+        }
+        val idx = blocks.indexOfFirst { it is IdBlockWithSize && it.id == id }
+        val sizeOfBlockToProcess = blocks[idx].size
+        val freeSpaceIdx = blocks.indexOfFirst { it is FreeSpaceBlockWithSize && it.size >= sizeOfBlockToProcess }
+        if (freeSpaceIdx in 0 until idx) {
+            val blockToProcess = blocks.removeAt(idx)
+            blocks.add(idx, FreeSpaceBlockWithSize(blockToProcess.size))
+            val spaceAvailable = blocks[freeSpaceIdx].size
+            blocks.removeAt(freeSpaceIdx)
+            if (spaceAvailable > sizeOfBlockToProcess) {
+                blocks.add(freeSpaceIdx, FreeSpaceBlockWithSize(spaceAvailable - sizeOfBlockToProcess))
+            }
+            blocks.add(freeSpaceIdx, blockToProcess)
+        }
+        return runFragmentor(blocks, id - 1)
+    }
+
     fun calculateChecksum(blocks: List<Block>): Long {
         return blocks.mapIndexed { idx, block ->
             (block as IdBlock).id * idx
         }.sum()
     }
 
+    private tailrec fun calculateChecksum(blocks: List<BlockWithSize>, blockIdx: Int = 0, positionIdx: Int = 0, sum: Long = 0): Long {
+        if (blockIdx == blocks.size) {
+            return sum
+        }
+
+        val block = blocks[blockIdx]
+        return if (block is IdBlockWithSize) {
+            val blockSum = (0 until block.size).sumOf { block.id * (positionIdx + it) }
+            calculateChecksum(blocks, blockIdx + 1, positionIdx + block.size, sum + blockSum)
+        } else {
+            calculateChecksum(blocks, blockIdx + 1, positionIdx + block.size, sum)
+        }
+    }
+
     fun part1(): Long {
         val expandedBlock = expandBlock(input)
-        val fragmented = runFragmenter(expandedBlock)
-//        println(fragmented.joinToString(""))
+        val fragmented = runFragmentor(expandedBlock)
+        return calculateChecksum(fragmented)
+    }
+
+    fun part2(): Long {
+        val expandedBlock = LinkedList(expandBlocksWithSize(input))
+        val fragmented = runFragmentor(expandedBlock, (expandedBlock.last as IdBlockWithSize).id)
         return calculateChecksum(fragmented)
     }
 
     interface Block
-    class FreeSpaceBlock: Block
-    class IdBlock(val id: Long): Block
+    object FreeSpaceBlock : Block
+    data class IdBlock(val id: Long) : Block
+
+    abstract class BlockWithSize(val size: Int) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as BlockWithSize
+
+            return size == other.size
+        }
+
+        override fun hashCode(): Int {
+            return size
+        }
+    }
+
+    class FreeSpaceBlockWithSize(size: Int) : BlockWithSize(size) {
+        override fun toString(): String {
+            return ".".repeat(size)
+        }
+    }
+
+    class IdBlockWithSize(val id: Long, size: Int) : BlockWithSize(size) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+            if (!super.equals(other)) return false
+
+            other as IdBlockWithSize
+
+            return id == other.id
+        }
+
+        override fun hashCode(): Int {
+            var result = super.hashCode()
+            result = 31 * result + id.hashCode()
+            return result
+        }
+
+        override fun toString(): String {
+            return "$id".repeat(size)
+        }
+    }
 }
 
 fun main() {
-    val diskfragmentor = DiskFragmentor(readInput(9))
-    println(diskfragmentor.part1())
-    //println(diskfragmentor.part2())
+    val diskFragmentor = DiskFragmentor(readInput(9))
+    println(diskFragmentor.part1())
+    println(diskFragmentor.part2())
 }
--- aoc-2024/src/test/kotlin/day09/DiskFragmentorTest.kt
+++ aoc-2024/src/test/kotlin/day09/DiskFragmentorTest.kt
@@ -1,8 +1,11 @@
 package day09
 
+import day09.DiskFragmentor.FreeSpaceBlock
+import day09.DiskFragmentor.IdBlock
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import utils.readInput
+import utils.readInputLines
 import utils.splitToString
 
 class DiskFragmentorTest {
@@ -11,38 +14,53 @@ class DiskFragmentorTest {
 
     @Test
     fun `should expand a simple block`() {
-        assertThat(testSubject.expandBlock("12345")).isEqualTo("0..111....22222")
+        assertThat(testSubject.expandBlock("12345"))
+            .isEqualTo("0..111....22222".toBlocks())
     }
 
     @Test
     fun `should expand a complex block`() {
-        assertThat(testSubject.expandBlock("2333133121414131402")).isEqualTo("00...111...2...333.44.5555.6666.777.888899")
+        assertThat(testSubject.expandBlock("2333133121414131402"))
+            .isEqualTo("00...111...2...333.44.5555.6666.777.888899".toBlocks())
     }
 
-//    @Test
-//    fun `should fragment simple block correctly`() {
-//        assertThat(testSubject.runFragmenter("0..111....22222")).isEqualTo("022111222")
-//    }
-//
-//    @Test
-//    fun `should fragment complex block correctly`() {
-//        assertThat(testSubject.runFragmenter("00...111...2...333.44.5555.6666.777.888899")).isEqualTo("0099811188827773336446555566")
-//    }
-//
-//    @Test
-//    fun `should calculate checksum correctly`() {
-//        assertThat(testSubject.calculateChecksum("0099811188827773336446555566".splitToString())).isEqualTo(1928)
-//    }
+    @Test
+    fun `should fragment simple block correctly`() {
+        assertThat(testSubject.runFragmentor("0..111....22222".toBlocks()))
+            .isEqualTo("022111222".toBlocks())
+    }
+
+    @Test
+    fun `should fragment complex block correctly`() {
+        assertThat(testSubject.runFragmentor("00...111...2...333.44.5555.6666.777.888899".toBlocks()))
+            .isEqualTo("0099811188827773336446555566".toBlocks())
+    }
+
+    @Test
+    fun `should calculate checksum correctly`() {
+        assertThat(testSubject.calculateChecksum("0099811188827773336446555566".toBlocks()))
+            .isEqualTo(1928)
+    }
 
     @Test
     fun `should get correct answer for part 1`() {
-         assertThat(DiskFragmentor(readInput(9)).part1())
+        assertThat(DiskFragmentor(readInput(9)).part1())
             .isEqualTo(6435922584968)
     }
 
     @Test
+    fun `should expand part 2 correctly`() {
+        assertThat(DiskFragmentor("2333133121414131402").part2())
+            .isEqualTo(2858)
+    }
+
+    @Test
     fun `should get correct answer for part 2`() {
-        // assertThat(DiskFragmentor(readInputLines(9)).part2())
-        //    .isEqualTo()
+         assertThat(DiskFragmentor(readInput(9)).part2())
+            .isEqualTo(6469636832766)
+    }
+
+    private fun String.toBlocks(): List<DiskFragmentor.Block> {
+        return splitToString().map { if (it == ".") FreeSpaceBlock else IdBlock(it.toLong()) }
     }
 }
