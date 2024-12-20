22a6d610dce74bb7f089504550eb84e44b9f6618
David Williams <davwil00@gmail.com>
2024-12-20 18:25:55 +0100

    Day 12 part 1

--- aoc-2024/src/main/kotlin/day12/GardenGroups.kt
+++ aoc-2024/src/main/kotlin/day12/GardenGroups.kt
@@ -1,14 +1,56 @@
 package day12
 
+import utils.Coordinate
+import utils.readGrid
 import utils.readInputLines
 
-class GardenGroups(input: List<String>) {
+class GardenGroups(private val map: Map<Coordinate, String>) {
+    fun part1(): Int {
+        val allCoordinatesInMap = map.keys.toMutableSet()
+        val regions = exploreRegions(allCoordinatesInMap)
+        return regions.sumOf { it.getPrice(map) }
+    }
 
+    private tailrec fun exploreRegions(coordinatesToExplore: Set<Coordinate>, regions: List<Region> = emptyList()): List<Region> {
+        if (coordinatesToExplore.isEmpty()) {
+            return regions
+        }
+
+        val nextCoordinateToExplore = coordinatesToExplore.first()
+        val plantType = map.getValue(nextCoordinateToExplore)
+        val region = exploreRegion(plantType, setOf(nextCoordinateToExplore))
+        return exploreRegions(coordinatesToExplore - region.coordinates, regions + region)
+    }
+
+    private tailrec fun exploreRegion(plantType: String, coordinatesToExplore: Set<Coordinate>, regionCoordinates: Set<Coordinate> = emptySet()): Region {
+        if (coordinatesToExplore.isEmpty()) {
+            return Region(plantType, regionCoordinates)
+        }
+
+        val currentCoordinate = coordinatesToExplore.first()
+        val adjacentCoordinatesInRegion = currentCoordinate.getAdjacentCoordinates()
+            .filterNot { it in regionCoordinates }
+            .filter { map[it] == plantType }
+        return exploreRegion(plantType, coordinatesToExplore - currentCoordinate + adjacentCoordinatesInRegion, regionCoordinates + currentCoordinate)
+    }
+
+    data class Region(val plantType: String, val coordinates: Set<Coordinate>) {
+        private fun calculatePerimeter(map: Map<Coordinate, String>): Int {
+            return coordinates.sumOf { coordinate ->
+                val adjacentCoordinates = coordinate.getAdjacentCoordinates(Int.MIN_VALUE, Int.MIN_VALUE)
+                adjacentCoordinates.count {
+                    map[it] == null || map[it] != plantType
+                }
+            }
+        }
+
+        fun getPrice(map: Map<Coordinate, String>) = calculatePerimeter(map) * coordinates.size
+    }
 
 }
 
 fun main() {
-    val gardenGroups = GardenGroups(readInputLines(12))
-//    println(gardenGroups.part1())
+    val gardenGroups = GardenGroups(readGrid(readInputLines(12)))
+    println(gardenGroups.part1())
 //    println(gardenGroups.part2())
 }
--- aoc-2024/src/test/kotlin/day12/GardenGroupsTest.kt
+++ aoc-2024/src/test/kotlin/day12/GardenGroupsTest.kt
@@ -1,26 +1,66 @@
 package day12
 
-import day13.ClawContraption
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import utils.readGrid
+import utils.readInputLines
 
 class GardenGroupsTest {
-    private val testInput = """""".lines()
+    private val simpleTestInput = """
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+    """.trimIndent().lines()
 
-    private val testSubject = ClawContraption(testInput)
+    private val complexTestInput = """
+        OOOOO
+        OXOXO
+        OOOOO
+        OXOXO
+        OOOOO
+    """.trimIndent().lines()
+
+    private val largerInput = """
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
+    """.trimIndent().lines()
 
     @Test
-    fun `should`() {
+    fun `should calculate the price of the fence for the simple input`() {
+        val testSubject = GardenGroups(readGrid(simpleTestInput))
+        assertThat(testSubject.part1()).isEqualTo(140)
+    }
+
+    @Test
+    fun `should calculate the price of the fence for the complex input`() {
+        val testSubject = GardenGroups(readGrid(complexTestInput))
+        assertThat(testSubject.part1()).isEqualTo(772)
+    }
+
+    @Test
+    fun `should calculate the price of the fence for the larger input`() {
+        val testSubject = GardenGroups(readGrid(largerInput))
+        assertThat(testSubject.part1()).isEqualTo(1930)
     }
 
     @Test
     fun `should get correct answer for part 1`() {
-        // assertThat(ClawContraption(readInputLines(13)).part1())
-        //    .isEqualTo()
+         assertThat(GardenGroups(readGrid(readInputLines(12))).part1())
+            .isEqualTo(1533644)
     }
 
     @Test
     fun `should get correct answer for part 2`() {
-        // assertThat(ClawContraption(readInputLines(13)).part2())
+        // assertThat(GardenGroups(readGrid(readInputLines(12))).part2())
         //    .isEqualTo()
     }
 }
