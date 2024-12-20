13d9c7e90781d1c5aa90c0b8d32f5c952886fd6f
David Williams <davwil00@gmail.com>
2024-12-20 16:00:07 +0100

    Day 11 part 2

--- aoc-2024/src/main/kotlin/day11/PlutonianPebbles.kt
+++ aoc-2024/src/main/kotlin/day11/PlutonianPebbles.kt
@@ -2,7 +2,6 @@ package day11
 
 import utils.isEven
 import utils.readInput
-import utils.readInputLines
 
 class PlutonianPebbles(input: String) {
 
@@ -14,21 +13,21 @@ class PlutonianPebbles(input: String) {
             stones = stones.flatMap {
                 applyStoneRules(it)
             }
+//            println(stones.groupingBy { it }.eachCount().toSortedMap())
         }
         return stones.size
     }
 
-    fun part2(): Int {
-        var stones = initialArrangement
+    fun part2(): Long {
+        val stones = initialArrangement.associateWith { 1L }.toMutableMap()
         repeat(75) {
-            stones = stones.flatMap {
-                applyStoneRules(it)
-            }
+            applyStoneRules(stones)
+//            println(stones.filterValues { it != 0L }.toSortedMap())
         }
-        return stones.size
+        return stones.values.sum()
     }
 
-    fun applyStoneRules(stoneValue: Long): List<Long> {
+    private fun applyStoneRules(stoneValue: Long): List<Long> {
         return when {
             stoneValue == 0L -> listOf(1)
             canSplit(stoneValue) -> splitStone(stoneValue)
@@ -36,17 +35,43 @@ class PlutonianPebbles(input: String) {
         }
     }
 
-    fun splitStone(stoneValue: Long): List<Long> {
+    fun applyStoneRules(map: MutableMap<Long, Long>) {
+        val entries = map.entries.map { (key, value) -> key to value }
+        entries.filterNot { it.second == 0L }
+            .forEach { (stoneValue, numberOfStones) ->
+                map.decrement(stoneValue, numberOfStones)
+                when {
+                    stoneValue == 0L -> map.increment(1, numberOfStones)
+                    canSplit(stoneValue) -> splitStone(stoneValue, map, numberOfStones)
+                    else -> map.increment(stoneValue * 2024, numberOfStones)
+                }
+            }
+    }
+
+    private fun splitStone(stoneValue: Long): List<Long> {
         val valueAsString = stoneValue.toString()
         val split = valueAsString.chunked(valueAsString.length / 2)
         return split.map { it.toLong() }
     }
 
+    private fun splitStone(stoneValue: Long, map: MutableMap<Long, Long>, numberOfStones: Long) {
+        val valueAsString = stoneValue.toString()
+        val split = valueAsString.chunked(valueAsString.length / 2)
+        return split.forEach { map.increment(it.toLong(), numberOfStones) }
+    }
+
     private fun canSplit(stoneValue: Long) = stoneValue.toString().length.isEven()
+    private fun MutableMap<Long, Long>.increment(key: Long, amount: Long = 1) {
+        put(key, getOrDefault(key, 0) + amount)
+    }
+
+    private fun MutableMap<Long, Long>.decrement(key: Long, amount: Long) {
+        put(key, getValue(key) - amount)
+    }
 }
 
 fun main() {
-    val plutonianpebbles = PlutonianPebbles(readInput(11))
-    println(plutonianpebbles.part1())
-    println(plutonianpebbles.part2())
+    val plutonianPebbles = PlutonianPebbles(readInput(11))
+    println(plutonianPebbles.part1())
+    println(plutonianPebbles.part2())
 }
--- aoc-2024/src/test/kotlin/day11/PlutonianPebblesTest.kt
+++ aoc-2024/src/test/kotlin/day11/PlutonianPebblesTest.kt
@@ -2,6 +2,7 @@ package day11
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import utils.readInput
 import utils.readSingleInputLineOfIntsFromCsv
 
 class PlutonianPebblesTest {
@@ -15,14 +16,36 @@ class PlutonianPebblesTest {
     }
 
     @Test
+    fun `should find correct number of stones after 6 iterations for test input with part2`() {
+        assertThat(testSubject.part2()).isEqualTo(22)
+    }
+
+    @Test
+    fun oneit() {
+        val input = "512 72 2024 2 0 2 4 2867 6032"
+        val testSubject = PlutonianPebbles(input)
+        val map = buildMap<Long, Long> {
+            put(512, 1)
+            put(72, 1)
+            put(2024, 1)
+            put(2, 2)
+            put(0, 1)
+            put(4, 1)
+            put(2867, 1)
+            put(6032, 1)
+        }.toMutableMap()
+        testSubject.applyStoneRules(map)
+    }
+
+    @Test
     fun `should get correct answer for part 1`() {
-        // assertThat(PlutonianPebbles(readInputLines(11)).part1())
-        //    .isEqualTo()
+         assertThat(PlutonianPebbles(readInput(11)).part1())
+            .isEqualTo(200446)
     }
 
     @Test
     fun `should get correct answer for part 2`() {
-        // assertThat(PlutonianPebbles(readInputLines(11)).part2())
-        //    .isEqualTo()
+         assertThat(PlutonianPebbles(readInput(11)).part2())
+            .isEqualTo(238317474993392)
     }
 }
